8118950c64f3d1e940a1387a551340a1aee8109f
Guilherme <145510603+Guilhermep7@users.noreply.github.com>
2025-03-16 21:53:01 -0300

    adding weather project

--- js/data.js
+++ js/data.js
@@ -38,7 +38,7 @@ const ProjectsData = [
       id: 1,
       name: 'Odontana',
       description: 'Odontana é um site para uma clínica odontológica que busca apresentar seus serviços no ambiente virtual. Inclui um formulário de agendamento, animações com ScrollReveal e é construído com React, oferecendo uma interface moderna e responsiva.',
-      technologies: ['React.Js', 'Next.Js', 'Typescript', 'Css'],
+      technologies: ['React.Js', 'Next.Js', 'Typescript', 'CSS'],
       url: 'https://odontana.vercel.app',
       github: 'https://github.com/guilhermep3/dentista-website'
    },
@@ -46,7 +46,7 @@ const ProjectsData = [
       id: 2,
       name: 'Clube do Sushi',
       description: 'Clube do Sushi é um site de restaurante com uma página inicial de apresentação e uma seção de pedidos. Desenvolvido com a biblioteca shadcn do React, oferece uma interface simples e funcional para o menu e simular pedidos online.',
-      technologies: ['React.Js', 'Next.Js', 'Typescript', 'Css'],
+      technologies: ['React.Js', 'Next.Js', 'Typescript', 'CSS'],
       url: 'https://clube-do-sushi.vercel.app',
       github: 'https://github.com/guilhermep3/clube-do-sushi'
    },
@@ -54,7 +54,7 @@ const ProjectsData = [
       id: 3,
       name: 'Web Cinema',
       description: 'Web Cinema é um site que simula uma plataforma de cinema, exibindo os filmes em estreia e em destaque, junto de uma ferramenta de pesquisa. O projeto também possui um simples sistema de login usando as bibliotecas react hook form e zod para o formulário.',
-      technologies: ['React.Js', 'Next.Js', 'Typescript', 'Css'],
+      technologies: ['React.Js', 'Next.Js', 'Typescript', 'CSS'],
       url: 'https://web-cinema.vercel.app/',
       github: 'https://github.com/guilhermep3/web-cinema'
    },
@@ -62,7 +62,7 @@ const ProjectsData = [
       id: 4,
       name: 'LevelUP',
       description: 'LevelUP é uma landing page para uma academia, projetada para atrair clientes com informações sobre serviços, planos e contato. O projeto se destaca com um design limpo e responsivo com animações sutis.',
-      technologies: ['React.Js', 'Next.Js', 'Typescript', 'Css'],
+      technologies: ['React.Js', 'Next.Js', 'Typescript', 'CSS'],
       url: 'https://levelup-academia.vercel.app/',
       github: 'https://github.com/guilhermep3/levelup-academia'
    },
@@ -70,7 +70,7 @@ const ProjectsData = [
       id: 5,
       name: 'Afazeres',
       description: 'Afazeres é uma aplicação de lista de tarefas desenvolvida com React e Next.js, projetada para ir além do comum ao oferecer mais personalização e organização para suas tarefas diárias.',
-      technologies: ['React.Js', 'Next.Js', 'Typescript', 'Css'],
+      technologies: ['React.Js', 'Next.Js', 'Typescript', 'CSS'],
       url: 'https://afazeres-todo.vercel.app/',
       github: 'https://github.com/guilhermep3/todo-list'
    },
@@ -84,9 +84,17 @@ const ProjectsData = [
    },
    {
       id: 7,
+      name: 'Clima Weather',
+      description: 'Clima Weather é um projeto feito consumindo a api openweathermap que disponibiliza dados de cidades.',
+      technologies: ['HTML', 'CSS', 'Javascript'],
+      url: 'https://guilhermep3.github.io/clima-weather/',
+      github: 'https://github.com/guilhermep3/clima-weather'
+   },
+   {
+      id: 8,
       name: 'Corte Clássico',
       description: 'Corte Clássico é uma landing page para uma barbearia, focada em converter mais clientes através de seu design simples e objetivo.',
-      technologies: ['React.Js', 'Next.Js', 'Typescript', 'Css'],
+      technologies: ['React.Js', 'Next.Js', 'Typescript', 'CSS'],
       url: 'https://corte-classico.vercel.app/',
       github: 'https://github.com/guilhermep3/barbearia-landing-page'
    },
