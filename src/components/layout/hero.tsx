0625902434fcd7480ae26714dec2c84304248b0b
Guilherme <145510603+Guilhermep7@users.noreply.github.com>
2026-01-20 11:39:18 -0300

    feat: update curriculum

--- src/components/layout/hero.tsx
+++ src/components/layout/hero.tsx
@@ -28,8 +28,8 @@ export const Hero = () => {
           <p data-aos="fade-up" data-aos-delay="2000"
             className="text-sm md:text-base mt-4 mb-8 max-w-xl ">Desenvolvedor Front-End. Construindo Universos Digitais, Transformando Ideias em Realidade</p>
           <div data-aos="fade-up" data-aos-delay="2000">
-            <Link href={'/curriculo-guilherme-pereira-dev-frontend.pdf'}
-              download={'curriculo-guilherme-pereira-dev-frontend'}
+            <Link href={'/curriculo-guilherme-pereira-dev.pdf'}
+              download={'curriculo-guilherme-pereira-dev'}
               className="buttonStyle boxShadowStyle"
             >Download CV</Link>
           </div>
